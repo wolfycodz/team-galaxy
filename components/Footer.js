48043cb7f0e1b621a1a082a2903aeefc5c0617eb
wolfy <103476206+wolfycodz@users.noreply.github.com>
2024-03-02 20:31:05 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="/Group 5logo_tg.png" alt="team galaxy logo" className={styles.logo} />
       </footer>
     </>
   )
