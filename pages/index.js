8e1382cd718a4b28ed74ffb9550c4fe07a548ba4
wolfy <103476206+wolfycodz@users.noreply.github.com>
2024-03-02 20:21:07 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Team galaxy!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         "we strive to make GOOD os's."
         </p>
       </main>
 
