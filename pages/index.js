45e94b249cd0cf22b8b9265f430169d9a1d96ed0
wolfy <103476206+wolfycodz@users.noreply.github.com>
2024-03-02 23:27:34 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <titleTeam galaxy</title>
+        <title "Team galaxy"</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
