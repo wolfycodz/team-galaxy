5964125494840d8ec6403cb76291f2e48cd7a78d
wolfy <103476206+wolfycodz@users.noreply.github.com>
2024-03-02 20:54:25 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,11 @@
-export default function Header({ title }) {
-  return <h1 src="/Group 5logo_tg_header.png" alt="team galaxy logo" className={styles.logo}</h1>
+import styles from './Header.module.css'
+
+export default function Header() {
+  return (
+    <>
+      <header className={styles.Header}>
+        <img src="/Group 5logo_tg_header.png" alt="team galaxy logo" className={styles.logo} />
+      </Header>
+    </>
+  )
 }
