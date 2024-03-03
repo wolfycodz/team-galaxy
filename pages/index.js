ade914369d29ae9a3a971ed70edcc93e402b4581
wolfy <103476206+wolfycodz@users.noreply.github.com>
2024-03-02 23:54:35 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,17 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title title="Team galaxy </title>
+        <title>Team galaxy</title>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
 
       <main>
         <Header title="Welcome to Team galaxy!" />
         <p className="description">
-         "we strive to make a better os."
-    <Link href="orbit-os.netlify.com">
-      <a>Orbit OS beta</a>
-    </Link>
+          We strive to make a better os.
+          <Link href="https://orbit-os.netlify.com" passHref>
+            <a>Orbit OS beta</a>
+          </Link>
         </p>
       </main>
 
