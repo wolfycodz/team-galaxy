1f67ffa1cb8e8c2b8e1e6ea63d876f8c4292467e
wolfy <103476206+wolfycodz@users.noreply.github.com>
2024-03-02 23:25:36 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <titleTeam galaxy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -14,7 +15,9 @@ export default function Home() {
         <Header title="Welcome to Team galaxy!" />
         <p className="description">
          "we strive to make a better os."
-          cy.contains('Orbit OS', 'https://orbit-os.netlify.app').click();
+    <Link href="orbit-os.netlify.com">
+      <a>Orbit OS beta</a>
+    </Link>
         </p>
       </main>
 
